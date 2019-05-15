46c5cb51b9480e95a8e6f80ba0bb6ae0d50030f6
coolsquid <9624459+coolsquid@users.noreply.github.com>
2019-05-16 00:00:33 +0200

    Updated the version number to 2.0.0.

--- src/main/java/coolsquid/toxicrain/ToxicRain.java
+++ src/main/java/coolsquid/toxicrain/ToxicRain.java
@@ -32,7 +32,7 @@ public class ToxicRain {
 
 	public static final String MODID = "toxicrain";
 	public static final String NAME = "ToxicRain";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "2.0.0";
 	public static final String DEPENDENCIES = "required-after:forge@[14.21.1.2387,)";
 	public static final String UPDATE_JSON =
 			"https://gist.githubusercontent.com/coolsquid/6b48d527776a66c644e1ce1b3c2776ae/raw/ee5b59727ebe6fc720a7104975ab1f9991b849a4/toxicrain.json";
