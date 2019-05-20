a8e1aa67fd2d06d223abb56519d133bc5c6a98d5
coolsquid <9624459+coolsquid@users.noreply.github.com>
2019-05-20 20:18:52 +0200

    Replaced the min and max time options with ranges. Streamlined stuff.

--- src/main/java/coolsquid/toxicrain/config/ConfigManager.java
+++ src/main/java/coolsquid/toxicrain/config/ConfigManager.java
@@ -2,11 +2,13 @@ package coolsquid.toxicrain.config;
 
 import java.awt.Color;
 import java.io.File;
+import java.util.regex.Pattern;
 
 import coolsquid.toxicrain.ToxicRain;
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 public class ConfigManager {
 
@@ -38,8 +40,12 @@ public class ConfigManager {
 	public static Color snowColor;
 
 	public static float minMoonFullness, maxMoonFullness;
-	public static int minTime, maxTime;
-	public static float minRainingStrength, minThunderingStrength;
+	public static float minRainingStrength, minThunderingStrength, maxThunderingStrength;
+
+	// Considered using a BitSet, but a boolean array is faster.
+	// The additional memory use is of little importance, as the array is quite
+	// small.
+	public static boolean[] timeRanges;
 
 	public static boolean enableConfigGui;
 
@@ -58,16 +64,39 @@ public class ConfigManager {
 		checkTimeDivisor = config.getInt("checkTimeDivisor", "general", 5, 1, Integer.MAX_VALUE,
 				"ToxicRain checks whether the player should be poisoned once every xth tick. The performance impact of ToxicRain scales inversely with this value.");
 
+		Property timeRangesProperty = config.get("general", "timeRanges", new String[0]);
+		timeRangesProperty.setComment("Rain will only be toxic when the game time within one of these time ranges. Each list entry should be a number (123) or a range (0-123). The numbers must be between 0 and 23999.");
+		timeRangesProperty.setValidationPattern(Pattern.compile("^(\\d+-\\d+|\\d+)$"));
+		if (timeRangesProperty.getStringList().length == 0) {
+			timeRanges = null;
+		} else {
+			timeRanges = new boolean[24000];
+			for (String s : timeRangesProperty.getStringList()) {
+				String[] ss = s.split("-");
+				if (ss.length == 2) {
+					int min = Integer.parseInt(ss[0]), max = Integer.parseInt(ss[1]);
+					while (min <= max) {
+						timeRanges[min++] = true;
+					}
+				} else if (ss.length == 1) {
+					timeRanges[Integer.parseInt(s)] = true;
+				} else {
+					throw new RuntimeException("'timeRanges' should contain a number (123) or a range (0-123). \"" + s
+							+ "\" is not a valid value.");
+				}
+			}
+		}
+
 		minMoonFullness = config.getFloat("minFullness", "general.moon", 0, 0, 1,
 				"Rain will only be toxic when the moon is at least this full.");
 		maxMoonFullness = config.getFloat("maxFullness", "general.moon", 1, 0, 1,
 				"Rain will only be toxic when the moon is at most this full.");
-		minTime = config.getInt("minTime", "general.time", 0, 0, 23999,
-				"Rain will only be toxic when the in-game time is between minTime and maxTime.");
-		maxTime = config.getInt("maxTime", "general.time", 23999, 0, 23999,
-				"Rain will only be toxic when the in-game time is between minTime and maxTime.");
-		minRainingStrength = config.getFloat("minRainingStrength", "general", 0, 0, 1, "");
-		minThunderingStrength = config.getFloat("minThunderingStrength", "general", 0, 0, 1, "");
+		minRainingStrength = config.getFloat("minRainingStrength", "general", 0.2F, 0, 1,
+				"Rain strength scales from 0 to 1, and changes whenever rain fades in or out. Rain will only be toxic when it is at least this strong. In Vanilla, the world is only considered to experience rain when the rain strength is at least 0.2. Warning: if this is 0, ToxicRain will apply its effects even without rain!");
+		minThunderingStrength = config.getFloat("minThunderingStrength", "general.thunder", 0, 0, 1,
+				"Thunder strength scales from 0 to 1. Rain will only be toxic when the thunder strength is at least this high. In Vanilla, the world is only considered to experience rain when the rain strength is at least 0.9. Note that thunder strength is multiplied by rain strength.");
+		maxThunderingStrength = config.getFloat("maxThunderingStrength", "general.thunder", 0, 0, 1,
+				"Thunder strength scales from 0 to 1. Rain will only be toxic when the thunder strength is at most this high. In Vanilla, the world is only considered to experience rain when the rain strength is at least 0.9. Note that thunder strength is multiplied by rain strength.");
 
 		delayOnSpawn = config.getInt("delayOnSpawn", "grace_periods", 600, -1, Integer.MAX_VALUE,
 				"The delay / grace period, in ticks, until a recently spawned player can be poisoned by rain. -1 does nothing.");
@@ -131,5 +160,22 @@ public class ConfigManager {
 		} else {
 			return !ConfigManager.blacklist.contains(dim);
 		}
+	}
+
+	public static boolean isTimeValid(long time) {
+		if (timeRanges == null) {
+			return true;
+		}
+		// The world time occasionally exceeds 23999, for some reason
+		return timeRanges[time > 23999 ? 23999 : (int) time];
+	}
+
+	public static boolean areRainAndThunderValid(float rainingStrength, float thunderingStrength) {
+		return rainingStrength >= minRainingStrength && thunderingStrength >= minThunderingStrength
+				&& thunderingStrength <= maxThunderingStrength;
+	}
+
+	public static boolean isMoonPhaseValid(float moonPhaseFactor) {
+		return moonPhaseFactor >= minMoonFullness && moonPhaseFactor <= maxMoonFullness;
 	}
 }
--- src/main/java/coolsquid/toxicrain/util/ModEventHandler.java
+++ src/main/java/coolsquid/toxicrain/util/ModEventHandler.java
@@ -117,38 +117,29 @@ public class ModEventHandler {
 				return;
 			}
 			if (!(ConfigManager.enableAntidote && event.player.isPotionActive(ToxicRain.antidote))
-					&& ConfigManager.isPoisonousDimension(event.player.world.provider.getDimension())) {
-				float moonPhaseFactor = event.player.world.getCurrentMoonPhaseFactor();
-				long time = event.player.world.getWorldTime();
-				if (moonPhaseFactor >= ConfigManager.minMoonFullness && moonPhaseFactor <= ConfigManager.maxMoonFullness
-						&& time >= ConfigManager.minTime && time <= ConfigManager.maxTime) {
-					boolean rainAndThunderConditions = event.player.world.rainingStrength >= ConfigManager.minRainingStrength
-							&& event.player.world.thunderingStrength >= ConfigManager.minThunderingStrength;
-					if (event.player.isInWater() && ConfigManager.toxicWater) {
-						event.player.addPotionEffect(new PotionEffect(ToxicRain.effect, ConfigManager.duration,
-								ConfigManager.amplifier, false, ConfigManager.particles));
-					} else {
-						if (event.player.world.isRaining() && rainAndThunderConditions
-								&& event.player.world.canSeeSky(event.player.getPosition())
-								&& event.player.world.getPrecipitationHeight(event.player.getPosition())
-										.getY() <= event.player.posY) {
-							Biome biome = event.player.world.getBiome(event.player.getPosition());
-							if (biome.canRain() || biome.getEnableSnow() && ConfigManager.toxicSnow) {
-								event.player.addPotionEffect(new PotionEffect(ToxicRain.effect, ConfigManager.duration,
-										ConfigManager.amplifier, false, ConfigManager.particles));
-							}
+					&& ConfigManager.isPoisonousDimension(event.player.world.provider.getDimension())
+					&& ConfigManager.isMoonPhaseValid(event.player.world.getCurrentMoonPhaseFactor())
+					&& ConfigManager.isTimeValid(event.player.world.getWorldTime())) {
+				boolean rainAndThunderConditions = ConfigManager.areRainAndThunderValid(
+						event.player.world.rainingStrength, event.player.world.getThunderStrength(1.0F));
+				if (event.player.isInWater() && ConfigManager.toxicWater) {
+					event.player.addPotionEffect(new PotionEffect(ToxicRain.effect, ConfigManager.duration,
+							ConfigManager.amplifier, false, ConfigManager.particles));
+				} else {
+					if (rainAndThunderConditions && event.player.world.canSeeSky(event.player.getPosition())
+							&& event.player.world.getPrecipitationHeight(event.player.getPosition())
+									.getY() <= event.player.posY) {
+						Biome biome = event.player.world.getBiome(event.player.getPosition());
+						if (biome.canRain() || biome.getEnableSnow() && ConfigManager.toxicSnow) {
+							event.player.addPotionEffect(new PotionEffect(ToxicRain.effect, ConfigManager.duration,
+									ConfigManager.amplifier, false, ConfigManager.particles));
 						}
 					}
-					if (rainAndThunderConditions) {
-						if (!cap.areClientTweaksActive()) {
-							PacketManager.sendToxicityUpdate(true, (EntityPlayerMP) event.player);
-							cap.setClientTweaksActive(true);
-						}
-					} else {
-						if (cap.areClientTweaksActive()) {
-							PacketManager.sendToxicityUpdate(false, (EntityPlayerMP) event.player);
-							cap.setClientTweaksActive(false);
-						}
+				}
+				if (rainAndThunderConditions) {
+					if (!cap.areClientTweaksActive()) {
+						PacketManager.sendToxicityUpdate(true, (EntityPlayerMP) event.player);
+						cap.setClientTweaksActive(true);
 					}
 				} else {
 					if (cap.areClientTweaksActive()) {
